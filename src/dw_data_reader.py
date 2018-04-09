4d65aeb6e76e51d5a9427fc8a45550b38be8b48a
m <>
2018-04-09 19:36:24 +0100

    up

--- src/dw_data_reader.py
+++ src/dw_data_reader.py
@@ -68,7 +68,7 @@ def get_dataset(k=10):
     dev =   [e for e in dev   if e.get_aux_labels() != None and len(e.get_aux_labels()) > 0]
     test =  [e for e in test  if e.get_aux_labels() != None and len(e.get_aux_labels()) > 0]
     
-    labels = "<UNK>" + sorted(set([e.get_label() for e in train]))
+    labels = ["<UNK>"] + sorted(set([e.get_label() for e in train]))
     
     labels_map = {l : i for i,l in enumerate(labels)}
     for e in train + dev + test:
