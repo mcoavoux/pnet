d528f0b03f290dc38df46cbba265ffe26a8b9e0c
m <>
2018-05-03 15:47:32 +0100

    up

---
+++ src/train_baselines.sh
@@ -0,0 +1,6 @@
+
+for dataset in tp_fr tp_de tp_us tp_dk tp_uk
+do
+    mkdir -p baseline_${dataset} &&
+    python3 main.py baseline_${dataset} ${dataset} -i 16 -L 1 -l 64 -w 32 -W 64 --baseline > baseline_${dataset}/baseline_log.txt
+done
